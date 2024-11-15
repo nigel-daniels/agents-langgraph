58e2c22b01c7006feda427c58f48e02c8bce9a5a
nigel-daniels <nigel.daniels@me.com>
2024-11-15 15:10:33 -0800

    Working on lesson 5.

--- 5-human/3-timetravel.ts
+++ 5-human/3-timetravel.ts
@@ -12,14 +12,13 @@ import confirm from '@inquirer/confirm';
 // First define the memory for persistance
 const memory = SqliteSaver.fromConnString(':memory:');
 
-
 // Set up the tools
 const tools = [new TavilySearchResults({maxResults: 2})];
 
-// First we define the state
+// First we define the state (with a custom reducer)
 const AgentState = Annotation.Root({
 	messages: Annotation<BaseMessage[]>({
-		reducer: (left, right) => reduceMessages(left, right), // Now we use the function
+		reducer: (left, right) => reduceMessages(left, right), // See below for function
 		default: () => []
 	})
 });
@@ -38,11 +37,11 @@ const model = new ChatOpenAI({
 
 // Now we can construct our agent graph
 const graph = new StateGraph(AgentState)
-	.addNode('llm', callOpenAi)
-	.addNode('action', takeAction)
+	.addNode('llm', callOpenAi) // See below for function
+	.addNode('action', takeAction) // See below for function
 	.addConditionalEdges(
 		'llm',
-		existsAction,
+		existsAction, // See below for function
 		{true: 'action', false: END}
 	)
 	.addEdge('action','llm')
@@ -52,74 +51,70 @@ const graph = new StateGraph(AgentState)
 		interruptBefore: ['action']
 	});
 
-///// Human approval
 
-// Now lets call the agent with a question
-const messages1 = [new HumanMessage('What is the weather in sf?')];
+// Let's do what we did in the modify exercise
+const messages = [new HumanMessage('What is the weather in LA?')];
 
-const config1 = {
-	configurable: {thread_id: '1'} // Used by memory to keep the converstion going
+const config = {
+	configurable: {thread_id: '3'}
 };
 
-// Start the agent (we will stop before calling the tool)
-for await (const event of await graph.stream({messages: messages1}, config1)) {
+for await (const event of await graph.stream({messages: messages}, config)) {
+	for (const [node, values] of Object.entries(event)) {
+		console.log('\nStreaming:')
+		console.log(values.messages);
+	}
+}
+
+let currentValues = await graph.getState(config);
+
+// Now let's replace the tool call with our own input
+currentValues.values.messages[currentValues.values.messages.length-1].tool_calls[0].args.input = 'current weather in Louisiana';
+
+await graph.updateState(config, currentValues.values);
+
+for await (const event of await graph.stream(null, config)) {
 	for (const [node, values] of Object.entries(event)) {
 		console.log(values.messages);
 	}
 }
 
-// Let's examine the state of the graph at the interrunpted point
-const state = await graph.getState(config1);
-console.log(state);
-console.log(state.next);
+// At this point we should have a result with the weather in Louisiana
+const states = [];
 
+const stateHistory = await graph.getStateHistory(config);
 
-///// Continue after interrupt
-/*
-// Now we can let it complete by passing in null as a message, the threat_id in the config
-// associates this with the state of the graph and lets it continue
-for await (const event of await graph.stream(null, config1)) {
-	for (const [node, values] of Object.entries(event)) {
-		console.log(values);
-	}
+for await (const state of stateHistory) {
+	console.log(state);
+	console.log('--');
+	states.push(state);
 }
-*/
 
-/*
-// Now let's rerun this and allow for decisions to be made
-const messages2 = [new HumanMessage('What is the weather in LA?')];
+// Let's get the state from our history in which we wanted to
+// call the search engine and ask about the weather in LA
+const toReplay = states[states.length-3];
+console.log(toReplay);
 
-const config2 = {
-	configurable: {thread_id: '2'} // We use a new thread
-};
-
-for await (const event of await graph.stream({messages: messages2}, config2)) {
+// Now we can run things forward from that state using toReplay.config
+for await (const event of await graph.stream(null, toReplay.config)) {
 	for (const [node, values] of Object.entries(event)) {
 		console.log(values.messages);
 	}
 }
 
+// We could also edit that historical message (for example suggest we get the weather from accuweather)
+toReplay.values.messages[toReplay.values.messages.length-1].tool_calls[0].args.input = 'current weather in LA, accuweather';
 
-let state = await graph.getState(config2);
+const branchState = await graph.updateState(toReplay.config, toReplay.values);
 
-while (state.next != '') {
-	console.log('\n' + state.next + '\n');
-
-	if (await confirm({ message: 'proceed?' })) {
-		for await (const event of await graph.stream(null, config2)) {
-			for (const [node, values] of Object.entries(event)) {
-				console.log(values);
-			}
-		}
-	} else {
-		console.log('aborting.');
-		break;
+for await (const event of await graph.stream(null, branchState)) {
+	for (const [node, values] of Object.entries(event)) {
+		console.log('node');
+		console.log(node);
+		console.log('values');
+		console.log(values.messages);
 	}
-	state = await graph.getState(config2);
 }
-*/
-
-
 
 ///////// FUNCTIONS USED BY THE GRAPH //////////
 
@@ -154,28 +149,33 @@ async function takeAction(state) {
 			const tool = tools.find(tool => tool.name === t.name);
 			result = await tool.invoke(t.args);
 		} else {
-			console.log('\n...bad tool name...');
+			//console.log('\n...bad tool name...');
 			result = 'bad tool name, retry';
 		}
 		results.push(new ToolMessage({tool_call_id: t.id, name: t.name, content: result.toString()}));
 	}
-	console.log('Back to the model!');
+	//console.log('Back to the model!');
 	return {messages: results};
 }
 
 // This function is used by the graph state to update the messages list
 // This lets us update previous messages based on id (or we add them)
 function reduceMessages(left: MessageUnion[], right: MessageUnion[]):MessaggeUnion[] {
+	// Ensure any new messages have an id
 	for (const message of right) {
-		if (!message.id) {
+		if (message.id === null || message.id === undefined) {
 			message.id = uuid4();
+			message.lc_kwargs.id = message.id;
 		}
 	}
 
+	// Copy the current set of messages
 	const merged = [...left];
 
+	// Now check to see if the new message exists in the curren set of messages
+	// if it does we update it if it does not we append it to the end ofg the array
 	for (const message of right) {
-		const i = merged.findIndex(element => element.id === message.id);
+		const i = merged.findIndex(existing => existing.id === message.id);
 
 		i !== -1 ? merged[i] = message : merged.push(message);
 	}
--- 5-human/notes.md
+++ 5-human/notes.md
@@ -1,2 +1,80 @@
 # Human in the Loop
-There are many instances were you want to provide human oversight of the agents work. This lesson builds on what we looked at in Lesson 4. 
+There are many instances were you want to provide human oversight of the agents work. This lesson builds on what we looked at in Lesson 4.
+
+## State memory
+As a graph is executed a snapshot of the state is stored in the memory:
+```
+     |
+     v
+    ---
+   |0,3|<-------
+    ---         |	      Memory
+     |          |        ---------
+     v          |       | State 3 |
+    / \         |        ---------
+   < 1 >        |       | State 2 |
+    \ /         |        ---------
+   / V \        |       | State 1 |
+  /     \       |        ---------
+ ---   ---      |       | State 0 |
+|   | | 2 |-----         ---------
+ ---   ---                   |
+                             V
+StateSnapshot: { AgentState, useful_things}
+                                   |
+                                   V
+                         {..., thread, thread_ts, ...}
+                                  |           |
+                                  V           |
+config = {configurable: {thread_id: '1',      V
+                         thread_ts: '1ef17b36-ed06-6185-8001-15cf75dea535'}};
+```
+The thread and the thread_ts (the threads UID) can be used to access the states:
+```
+config = {configurable: {thread_id: '1'}};              ---------    
+                            |                          | State 3 |
+                            V                           ---------
+            graph.getState({..., thread, ...}); Returns 'current state'
+```
+We can also get an iterator over all of the previous states:
+```
+                                                     ---------
+    graph.getStateHistory({..., thread, ...});      | State 3 |
+                                                     --------- |
+                                                      --------- |
+	Returns an iterator over        	               --------- |
+		all StateSnapshots				                 ---------
+```
+Using the iterator allows us to get individual snapshots with their UIDs `thread_ts` we can then use this to replay from a previous point:
+```
+    graph.invoke(null, {..., thread, thread_ts, ...});       ---------
+    graph.stream(null, {..., thread, thread_ts, ...});      | State n |
+                                                             ---------
+                                                     Run with state n as the
+                                                   starting point, Time Travel
+```
+If we do the above with out the `thread_ts` identifier we will use the current state as the starting point:
+```
+    graph.invoke(null, {..., thread, ...});       ---------
+    graph.stream(null, {..., thread, ...});      | State 3 |
+    											  ---------
+	                                        Uses the current state
+		                                     as the starting point
+```
+We can also use the `thread_ts` to access a previous state, we can then modify it and add the modified state back on the memory stack making it the current state:
+```
+                                                         ---------
+    graph.getState({..., thread, thread_ts, ...});      | State 1 |
+                                                         ---------
+                                                             |
+                                                             V
+                                                         ---------
+                    modify it                           | State m |
+                                                         ---------
+                                                             |
+                                                             V
+                                                         ---------
+	graph.updateState(thread, state_m.values);          | State 4 |
+                                                         ---------
+    graph.invoke(null, {..., thread, ...});   Runs state 4 as the current state.
+```
